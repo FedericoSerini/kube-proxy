50bad8820b9d1d4f97752bb14f6190e8f20ebd5c
FedericoSerini <federico.serini.se@pec.it>
2025-04-27 13:02:19 +0200

    update: crd client

--- pkg/kubernetes/kubeclient.go
+++ pkg/kubernetes/kubeclient.go
@@ -1,31 +1,53 @@
 package kubernetes
 
 import (
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
-// CreateClient initializes and returns a Kubernetes clientset
 func CreateClient() (*kubernetes.Clientset, error) {
 	var configPath string
 
-	// Use kubeconfig from the default location (if available)
 	if home := homedir.HomeDir(); home != "" {
 		configPath = home + "/.kube/config"
 	}
 
-	// Build Kubernetes client from the config file
 	config, err := clientcmd.BuildConfigFromFlags("", configPath)
 	if err != nil {
 		return nil, err
 	}
 
-	// Create a clientset using the config
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		return nil, err
 	}
 
 	return clientset, nil
+}
+
+func CreateClientCrd() (*kubernetes.Clientset, dynamic.Interface, error) {
+	var configPath string
+
+	if home := homedir.HomeDir(); home != "" {
+		configPath = home + "/.kube/config"
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", configPath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return clientset, dynamicClient, nil
 }
